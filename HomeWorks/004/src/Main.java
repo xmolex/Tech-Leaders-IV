3dc75916dba5129ce48f245fba2ae61c7fe1205c
xmolex <xmolex@list.ru>
2020-02-18 12:44:29 +0300

    homework 004: calculate Fibonacci

---
+++ HomeWorks/004/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        int ourNum = 77;
+
+        System.out.println("Calculate Fibonacci for " + ourNum + ":");
+        System.out.println( CalculateFibonacci.calculate(ourNum) );
+
+    }
+
+}
+
+// 3  = 2
+// 7  = 13
+// 77 = 5527939700884757
