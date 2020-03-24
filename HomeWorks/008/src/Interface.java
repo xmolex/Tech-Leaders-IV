1920a400cc778248e296f7a75270556795550304
Konstantin Titov <xmolex@list.ru>
2020-03-24 17:16:58 +0300

    homework: hashMap, add method List

--- HomeWorks/008/src/Interface.java
+++ HomeWorks/008/src/Interface.java
@@ -1,8 +1,10 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Interface {
 
     private final String COMMAND_EXIT = "Exit";
+    private final String COMMAND_LIST = "List";
 
     // инициализация запроса данных с консоли
     public void scan(Phones phones) {
@@ -11,15 +13,27 @@ public class Interface {
         System.out.println("Enter name or Exit");
         String cmdText = scanner.nextLine();
         while (!COMMAND_EXIT.equals(cmdText)) {
-            long phoneNumber;
 
-            try {
-                phoneNumber = phones.get(cmdText);
-                System.out.println("Phone number for " + cmdText + " is '" + phoneNumber + "'");
-            } catch (Exception e) {
-                System.out.println(cmdText + " not found");
+            if (COMMAND_LIST.equals(cmdText)) {
+                // выводим список из всех элементов в адресной книге
+                String[] names = phones.list();
+                System.out.println("Found elems: " + names.length);
+                for (int i = 0; i < names.length; i++) {
+                    System.out.println("name: " + names[i] + ", phone: " + phones.get(names[i]));
+                }
+            } else {
+                // поиск номера по имени
+                long phoneNumber;
+
+                try {
+                    phoneNumber = phones.get(cmdText);
+                    System.out.println("Phone number for " + cmdText + " is '" + phoneNumber + "'");
+                } catch (Exception e) {
+                    System.out.println(cmdText + " not found");
+                }
             }
 
+            // запрос новой порции данных
             System.out.println("Enter name or Exit");
             cmdText = scanner.nextLine();
         }
--- HomeWorks/008/src/Phones.java
+++ HomeWorks/008/src/Phones.java
@@ -1,4 +1,5 @@
 import java.util.HashMap;
+import java.util.Set;
 
 public class Phones {
 
@@ -10,6 +11,12 @@ public class Phones {
 
     public long get(String name) {
         return phoneBook.get(name);
+    }
+
+    // выводим список ключей
+    public String[] list() {
+        Set<String> keys = phoneBook.keySet();
+        return keys.toArray(new String[keys.size()]);
     }
 
 }
