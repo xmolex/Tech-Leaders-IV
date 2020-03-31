0fa46f7bc00829b2741e7fb6e1b1a9c981799ab5
Konstantin Titov <xmolex@list.ru>
2020-03-31 16:00:26 +0300

    homework: benchmark for collections: add 3 and 4 part

--- HomeWorks/009/src/Main.java
+++ HomeWorks/009/src/Main.java
@@ -8,6 +8,12 @@ public class Main {
 
         // добавление по фиксированному индексу (2 теста, в начало и конец) в ArrayList, LinkedList
         //testAddToBeginAndEnd(50000, 1000000);
+
+        // заполнить HashSet и TreeSet целыми числами. Проверить разницу в скорости по методу contains
+        //testFillAndContainsBySet(1000000);
+
+        // сравнить скорость contains HashSet, ArrayList, LinkedList
+        //testContains(50000);
     }
 
     // добавление элементов в ArrayList, LinkedList, HashSet, TreeSet
@@ -35,5 +41,26 @@ public class Main {
         System.out.println("test add to end ArrayList: " + TestUtils.testAddToDefinePosition(new ArrayList<>(), 0) + " ms");
         System.out.println("test add to end LinkedList: " + TestUtils.testAddToDefinePosition(new LinkedList<>(), 0) + " ms");
         System.out.println("Быстрее всего должно работать в LinkedList, т.к. просто добавляет новый объект (node) и устанавливает на него ссылки у соседа, а вот ArrayList постоянно смещает все элементы");
+    }
+
+    // заполнить HashSet и TreeSet целыми числами. Проверить разницу в скорости по методу contains
+    public static void testFillAndContainsBySet(int iterationsCount) {
+        TestUtils.setIterationsCount(iterationsCount);
+        System.out.println("=========== iterations count: " + iterationsCount + " ================" );
+
+        System.out.println("test add to HashSet: " + TestUtils.testFillAndContains(new HashSet<>()) + " ms");
+        System.out.println("test add to TreeSet: " + TestUtils.testFillAndContains(new TreeSet<>()) + " ms");
+        System.out.println("Не должно быть особой разницы, возможно немного быстрее в HashSet, т.к. алгоритм более проще, нет сортировки");
+    }
+
+    // сравнить скорость contains HashSet, ArrayList, LinkedList
+    public static void testContains(int iterationsCount) {
+        TestUtils.setIterationsCount(iterationsCount);
+        System.out.println("=========== iterations count: " + iterationsCount + " ================" );
+
+        System.out.println("test add to HashSet: " + TestUtils.testFillAndContainsInCollections(new HashSet<>()) + " ms");
+        System.out.println("test add to ArrayList: " + TestUtils.testFillAndContainsInCollections(new ArrayList<>()) + " ms");
+        System.out.println("test add to LinkedList: " + TestUtils.testFillAndContainsInCollections(new LinkedList<>()) + " ms");
+        System.out.println("Быстрее всего должно искать в HashSet, т.к. грубо говоря значение преобразуется в адрес node и можно сразу получить значение, а в array реализациях необходимо перебирать массив, пока не встретишь требуемое значение");
     }
 }
--- HomeWorks/009/src/TestUtils.java
+++ HomeWorks/009/src/TestUtils.java
@@ -1,7 +1,9 @@
 import com.sun.javaws.IconUtil;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+import java.util.Set;
 
 public class TestUtils {
 
@@ -28,6 +30,29 @@ public class TestUtils {
         return System.currentTimeMillis() - startTime;
     }
 
+    public static long testFillAndContains(Set<Integer> setList) {
+        for(int i = 0; i < ITERATIONS_COUNT; i++){
+            setList.add(i);
+        }
+
+        long startTime = System.currentTimeMillis();
+        for(int i = 0; i < ITERATIONS_COUNT; i++){
+            setList.contains(i);
+        }
+        return System.currentTimeMillis() - startTime;
+    }
+
+    public static long testFillAndContainsInCollections(Collection<Integer> collection) {
+        for(int i = 0; i < ITERATIONS_COUNT; i++){
+            collection.add(i);
+        }
+
+        long startTime = System.currentTimeMillis();
+        for(int i = 0; i < ITERATIONS_COUNT; i++){
+            collection.contains(i);
+        }
+        return System.currentTimeMillis() - startTime;
+    }
 
     public static void setIterationsCount(int iterationsCount) {
         ITERATIONS_COUNT = iterationsCount;
